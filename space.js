92ae1fe4269e082002ba3cfada3e51db548f0819
Arlene <kara_arlene@yahoo.com>
2018-03-21 19:15:34 -0600

    Basic files- moving setup to Python/Django
    Getting files with resources in.

---
+++ space.js
@@ -0,0 +1,11 @@
+//TODO picture carosel
+//TODO single page fake?
+//TODO social links
+//TODO stripe/checkout?
+//TODO inventory
+//TODO map link?
+//TODO colors
+
+// https://www.facebook.com/space-cadets-glass-and-accessories-121964391896864/?ref=br_rs
+//https://www.facebook.com/spacecadetsglass/?ref=br_rs
+// https://www.yelp.com/biz/space-cadets-glass-and-accessories-cheyenne
