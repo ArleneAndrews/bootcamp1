5b19c1652610ddda2c29863e47282b44831eb105
ArleneAndrews <Kara_arlene@yahoo.com>
2018-03-27 13:33:50 -0600

    pm commit

--- Pset6/mario.py
+++ Pset6/mario.py
@@ -1,7 +1,7 @@
 while True:
     print("How many levels? Max 23")
     levels = input()
-    if 0 < levels < 24:
+    if 0 < int(levels) < 24:
         continue
-    for i in range (levels, -1, -1):
-        print # 
+    for i in range (int(levels), -1, -1):
+        for j in range (1, int(levels), 1):
