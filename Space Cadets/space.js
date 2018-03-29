0d6a820a171575aca5b8510d9cb9367fe3107918
ArleneAndrews <Kara_arlene@yahoo.com>
2018-03-29 16:42:52 -0600

    Moveing things

---
+++ Space Cadets/space.js
@@ -0,0 +1,12 @@
+//TODO picture carosel
+//TODO single page fake?
+//TODO social links
+//TODO stripe/checkout?
+//TODO inventory
+//TODO map link?
+//TODO colors
+// Signup fun: https://growthmachine.typeform.com/signup?utm_campaign=nQGRmF&utm_source=typeform.com-226443-Basic&utm_medium=typeform&utm_content=typeform-thankyoubutton&utm_term=EN
+
+// https://www.facebook.com/space-cadets-glass-and-accessories-121964391896864/?ref=br_rs
+//https://www.facebook.com/spacecadetsglass/?ref=br_rs
+// https://www.yelp.com/biz/space-cadets-glass-and-accessories-cheyenne
