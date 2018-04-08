83c3f31876a16a53ca2138812d041b054f67d687
ArleneAndrews <Kara_arlene@yahoo.com>
2018-04-08 13:57:25 -0600

    Rook helpfulness

--- Pset6/mario.py
+++ Pset6/mario.py
@@ -6,14 +6,21 @@ elif levels > 24:
     print("Too tall! Less than 24, please.")
 
 else:
-    
-    brick ='#'
-    step =' '
+    brick =''
+    step = ''
     line = ''
+
     # This should print spaces, one less each iteration
-    for i in range(levels, -1, -1):
-       line += step
-    #This should print the hashes
+    for i in range(levels-1, -1, -1):
+       step += ' '
+    #This should print the hashes to complete the row
     for i in range(levels):
-        line += brick
+        line += '#'
         print(line)
+
+        """  for space in range(numberOfSpacesForCurrentRow):
+            currentRowAsString += " "
+        for brick in range(numberOfBricksForCurrentRow):
+            currentRowAsString += "#"
+
+        print(currentRowAsString) """
