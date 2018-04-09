a43a8b7a4bb00a5f734fdb98003844e246c70287
ArleneAndrews <Kara_arlene@yahoo.com>
2018-04-08 18:03:09 -0600

    \o/

--- Pset6/mario2.py
+++ Pset6/mario2.py
@@ -1,4 +1,4 @@
-levels = int(input("How many levels?"))
+levels = int(input("How many levels?  "))
 if 1 > levels:
     print("I can't print that type yet!")
 
@@ -6,14 +6,21 @@ elif levels > 24:
     print("Too tall! Less than 24, please.")
 
 else:
-    brick ='#'
-    step = '~'
-    line = ''
+    for level in range(levels, 0, -1):
+        #start of new row
+        line = ''
+        #calculate number of spaces for this level
+        numberOfSpacesForLevel = level - 1
+        #calculate number of bricks for this level
+        numberOfBricksForLevel = levels - numberOfSpacesForLevel
 
-    for space in range(levels):
-        line -= step
+        #add spaces to this new row
+        for space in range(numberOfSpacesForLevel):
+            line += ' '
+
+        #do bricks stuff here
+        for brick in range(numberOfBricksForLevel):
+            line += '#'
+
+        #Line has been built, print and then start the loop over
         print(line)
-    # for brick in range(levels):
-    #    currentRowAsString += "#"
-    #     print (line)
-    
