965ca4468b511e97acab75855a561bf944be75c4
ArleneAndrews <kara_arlene@yahoo.com>
2018-04-09 10:59:28 -0600

    New Webpage plus copy pasta

---
+++ Chop Shop/style.js
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>FarLynx-Add graphics</title>
+    <link rel="stylesheet" type="text/css" href="flstyle.css">
+    <script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <div id="navblock">
+        <a href="farlynx03.html" id="navbutton">Back</a>
+        <a href="farlynx00.html" id="navbutton">Start of sample</a>
+        <a href="index.html" id="navbutton-main">Back to main page</a>
+        <a href="farlynx05.html" id="navbutton">Next</a>
+    </div>
+
+    <aside class="pquote">Adding graphics, and changing some of the font styling will make information pop. Information was updated, since it was obvious that the main page could handle more. There is also going to be a graphic so you can select your area of the world and
+        get in contact with the correct representative. LINKS TO THE FORM PICTURE WAS AN ACTUAL PRODUCT. </aside>
+
+    <div id="open"><span id="jumbo">Welcome To FarLynx!</span></div>
+    <p><span class="inital-cap">B</span>uilding on our combined experiences, we have made FarLynx into a leader in the areas of silicon cooking tools, baking forms, and customization of kitchen modules. These things capitalize on the strengths each of out
+        founders have. Our passion is cooking, and producing the tools and spaces that make the process enjoyable, efficient, and delicious.</p>
+    <div id="vanilla">
+        <span class="inital-cap">O</span>ur products are researched, then tested in both institutions and nationally-recognized test kitchens to insure durability, ease of use, and address the specific ergonomic concerns of those who cook for a living.
+        This ensures that any FarLynx product you purchase will have many hours of real-world testing behind it.</div>
+    <div id="img">
+        <div class="row">
+            <div class="col">
+            </div>
+            <div class="col">
+                <img src="http://www.kempdesignservices.com/architecture/images/firekeeperscasinokitchen.jpg" height="100"><img>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+    <ul>
+        <li> Our kitchen modules are based on standard cabinet height, so we can use the basic construction plans to insure that the modules cover the area, and give you the largest amount of counter space. </li>
+        <ul style="list-style-type:circle">
+            <li>Pan storage</li>
+            <li>Pastry boards</li>
+            <li>Recessed storage</li>
+            <li>Drain and cooling areas</li>
+        </ul>
+        <div id="img">
+            <div class="row">
+                <div class="col">
+                </div>
+                <div class="col">
+                    <img src="http://res.cloudinary.com/kara/image/upload/c_scale,h_133/a_90/v1507915627/USD0496565-20040928-D00002_a0iu2w.png"><img>
+                </div>
+                <div class="col"></div>
+            </div>
+            <li>Tools are made from <a href="https://www.thespruce.com/silicone-bakeware-tips-1907152">silicon</a> and easy on your body to use, and are not going to melt: even when using some of them to turn food on a grill.
+                <ul style="list-style-type:circle">
+                    <li>Backyard grill tools</li>
+                    <li>Basic tool sets</li>
+                    <li>Custom colors and branding</li>
+                    <li>All tools egronomicly designed</li>
+                </ul>
+            </li>
+            <div id="img">
+                <div class="row">
+                    <div class="col">
+                    </div>
+                    <div class="col">
+                        <img src="https://www.dhresource.com/webp/m/0x0s/f2-albu-g3-M01-8D-73-rBVaHFZWdOmAOKhcAAGiyMTfCOI640.jpg/hot-sale-1pcs-3d-rose-flower-silicone-baking.jpg" width="100" height="100"><img>
+                    </div>
+                    <div class="col"></div>
+                </div>
+            </div>
+            <li>Baking forms that we do are the high-end, one of a kind forms that will usually not be reused. For this reason, we use a thin-walled, pre-treated form that will minimize sticking of both frozen and hot desserts. With the possibility of using
+                silicon for smaller ones.
+                <ul style="list-style-type:circle">
+                    <li>Multi-part forms</li>
+                    <li>Specialty cake pans</li>
+                    <li>One-time use forms</li>
+                    <li>Frozen and hot food stable</li>
+                </ul>
+            </li>
+    </ul>
+
+    <div id="about">About
+        <div class="row">
+            <div class="col">
+                Mike Garand started a business that has grown into FarLynx making pan forms back in high school. He has gone on to create many custom forms, including a large multi-piece one used to replicate Bart Simpson for the anniversary dinner. His work in the past
+                has been used as a basis for ice sculpting, the The San Francisco Ice Cream Festival, and custom small pies for the 150th birthday dinner at the Abe Lincoln Presidential Library Foundation. His first pan was one invented to assist in making
+                a chess cake.
+            </div>
+            <div class="col">
+                George G. Hamilton received his degree in Design in 1983, and has been working with many professional kitchens from then on. His work in designing our modular system of kitchen units has made a major difference in the availability of solutions in both
+                the professional and home markets. Many of his works have been used in professional publications, and the designs are incorporated into the kitchen designs in motor homes
+            </div>
+            <div class="col">
+                Deni Wilde took a dual major in Sculpture and Industrial Design, and has turned her attention to the creation of ergonomic, useful, and attractive cooking tools. The attention to detail has made her designs some of the most practical and attractive wares
+                found anywhere in the world. The tools she has designed have recieved recognition from federal agecies for the comfort and ease of use.
+            </div>
+        </div>
+    </div>
+
+    <div id="vanilla">To find our products, we partner with <a href="">GiftsForCooks</a> for the selection and price that make these affordable. Commercial accounts are serviced by one of our regional managers, who can provide you with a local account manager. Please select
+        your region, or click on the map.
+
+        <div id="img2">
+            <div class="row">
+                <div class="col">
+                    <div> North: <a href="">Greg Monahan</a>
+                        <p></p>
+                    </div>
+                    <div> South: <a href="">Lauren Wilson</a>
+                        <p></p>
+                    </div>
+                    <div> Islands: <a href="">Manaza Booth</a></div>
+                </div>
+                <div class="col">
+                    <img src="gondwana.jpg" usemap="#planetmap"></img>
+                    <map name="planetmap">
+                            <area shape="poly" coords="128, 15, 313, 15, 377, 68, 333, 121, 128, 133" href="" alt="North">
+                            <area shape="poly" coords="65, 191, 107, 130, 316, 130, 295, 215, 276, 300, 61, 221" href="" alt="South">
+                            <area shape="poly" coords="373,94, 540,168, 540,316, 277, 316, 283,268, 313,183, 309,148, 351,100" href="" alt="Islands">
+                          </map>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="navblock">
+        <a href="farlynx03.html" id="navbutton">Back</a>
+        <a href="farlynx00.html" id="navbutton">Start of sample</a>
+        <a href="index.html" id="navbutton-main">Back to main page</a>
+        <a href="farlynx05.html" id="navbutton">Next</a>
+    </div>
+</body>
+
+</html>
