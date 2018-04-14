ced09525b25f52202a8f8baf7851a1066cf4b66c
ArleneAndrews <kara_arlene@yahoo.com>
2018-04-14 11:39:58 -0600

    pseudo code

--- Pset6/ceasar.py
+++ Pset6/ceasar.py
@@ -1,2 +1,18 @@
-message = str(input("Please add your message here. "))
+# need loop for each?
+message = (input("Please add your message here. "))
+# need check for string here
+
 key = int(input("Choose a numerical key. "))
+#need check for numerals
+
+#keymapping
+
+#change to all uppeercase
+
+#dealing with space/punctuation
+
+#shifting
+
+#wrap around on shifting
+
+#print to screen
