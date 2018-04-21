40da77a36b0b6a15e9010848689f6eb71f68ce5c
ArleneAndrews <Kara_arlene@yahoo.com>
2018-04-21 15:58:34 -0600

    still closer

--- Pset6/ceasar.py
+++ Pset6/ceasar.py
@@ -1,24 +1,17 @@
 # need loop for each?
 message = (input("Please add your message here. "))
 messagelength = len(message)
-message = message.upper()
     
 key = int(input("Choose a numerical key. "))
-
-print("This works.")
-
 
 #Insure key is less than 26
 if key > 26:
     key = key % 26
 
-print(key)
-
-
 #keymapping
 def toCesaer(incoming):
     ceasar = ''
-    for letter in range(messagelength):
+    for letter in incoming:
         if 'A' <= letter <= 'Z':
             location = ord(letter)
             new_ascii = location + key
