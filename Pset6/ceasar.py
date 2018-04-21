828755fb383d0e90d8c9c45c53094e524aa4554b
ArleneAndrews <Kara_arlene@yahoo.com>
2018-04-21 15:49:58 -0600

    maybe works

--- Pset6/ceasar.py
+++ Pset6/ceasar.py
@@ -1,9 +1,12 @@
 # need loop for each?
 message = (input("Please add your message here. "))
+messagelength = len(message)
+message = message.upper()
     
 key = int(input("Choose a numerical key. "))
 
 print("This works.")
+
 
 #Insure key is less than 26
 if key > 26:
@@ -11,16 +14,16 @@ if key > 26:
 
 print(key)
 
+
 #keymapping
+def toCesaer(incoming):
+    ceasar = ''
+    for letter in range(messagelength):
+        if 'A' <= letter <= 'Z':
+            location = ord(letter)
+            new_ascii = location + key
+            character = chr(new_ascii)
+        ceasar = ceasar + character
+    return ceasar
 
-#change to all uppercase
-
-#slice into individual characters
-
-#dealing with space/punctuation
-
-#wrap around on shifting
-
-#shifting
-
-#print to screen
+print(toCesaer(message))
