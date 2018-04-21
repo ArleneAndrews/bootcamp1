374441bac45106b503522c70bc1265554682c43c
ArleneAndrews <Kara_arlene@yahoo.com>
2018-04-21 16:39:10 -0600

    \O/rks!

--- Pset6/ceasar.py
+++ Pset6/ceasar.py
@@ -3,6 +3,8 @@ message = (input("Please add your message here. "))
 messagelength = len(message)
     
 key = int(input("Choose a numerical key. "))
+if key <= 0:
+    print('Key must be larger than 0.')
 
 #Insure key is less than 26
 if key > 26:
@@ -17,7 +19,7 @@ def toCesaer(incoming):
             location = ord(letter)
             new_ascii = location + key
             if  ord('Z') < new_ascii:
-                new_ascii = int(new_ascii - ord ('A'))
+                new_ascii = int(((new_ascii - ord('A')) % 26) + ord('A'))
             character = chr(new_ascii)
         ceasar = ceasar + character
     return ceasar
