aa54224ba3280aff707b2d9be9285fc03e55161c
ArleneAndrews <Kara_arlene@yahoo.com>
2018-04-21 17:02:52 -0600

    Copy of Ceasar into Vigenere, to save typing

--- Pset6/vigenere.py
+++ Pset6/vigenere.py
@@ -1,20 +1,25 @@
 # need loop for each?
-message = (input("Please add your message here. "))
-# need check for string here
+   
+key = (input("Choose an alpabetical key. "))
 
-key = int(input("Choose a numerical key. "))
-#need check for numerals
+message = (input("Please add your message here. "))
+
+#Insure key is less than 26
+if key > 26:
+    key = key % 26
 
 #keymapping
+def toVigenere(incoming):
+    ceasar = ''
+    for letter in incoming:
+        letter = letter.upper()
+        if 'A' <= letter <= 'Z':
+            location = ord(letter)
+            new_ascii = location + key
+            if  ord('Z') < new_ascii:
+                new_ascii = int(((new_ascii - ord('A')) % 26) + ord('A'))
+            character = chr(new_ascii)
+        ceasar = ceasar + character
+    return ceasar
 
-#change to all uppercase
-
-#slice into individual characters
-
-#dealing with space/punctuation
-
-#wrap around on shifting
-
-#shifting
-
-#print to screen
+print(toVigenere(message))
