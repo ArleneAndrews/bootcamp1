3b910441484d229f33e15952186a6485e82408eb
ArleneAndrews <Kara_arlene@yahoo.com>
2018-04-22 08:54:42 -0600

    maybe?

--- Pset6/vigenere.py
+++ Pset6/vigenere.py
@@ -1,25 +1,21 @@
-# need loop for each?
-   
 key = (input("Choose an alpabetical key. "))
 
 message = (input("Please add your message here. "))
 
-#Insure key is less than 26
-if key > 26:
-    key = key % 26
-
 #keymapping
 def toVigenere(incoming):
-    ceasar = ''
+    cypher = ''
+    count = 0
+
     for letter in incoming:
         letter = letter.upper()
         if 'A' <= letter <= 'Z':
             location = ord(letter)
-            new_ascii = location + key
+            new_ascii = location + ord(key[count % key.len].upper())
             if  ord('Z') < new_ascii:
                 new_ascii = int(((new_ascii - ord('A')) % 26) + ord('A'))
             character = chr(new_ascii)
-        ceasar = ceasar + character
-    return ceasar
+        cypher = cypher + character
+    return cypher
 
 print(toVigenere(message))
