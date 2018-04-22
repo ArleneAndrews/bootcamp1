5ffd6b7d208669d6b913f595de1afef6ffc62670
ArleneAndrews <Kara_arlene@yahoo.com>
2018-04-22 09:51:14 -0600

    nope.

--- Pset6/vigenere.py
+++ Pset6/vigenere.py
@@ -11,10 +11,13 @@ def toVigenere(incoming):
         letter = letter.upper()
         if 'A' <= letter <= 'Z':
             location = ord(letter)
-            new_ascii = location + ord(key[count % key.len].upper())
+            shift = global.key(count)
+            print(shift)
+            new_ascii = location + ord(key[count % len(key)].upper())
             if  ord('Z') < new_ascii:
                 new_ascii = int(((new_ascii - ord('A')) % 26) + ord('A'))
             character = chr(new_ascii)
+            count += 1
         cypher = cypher + character
     return cypher
 
