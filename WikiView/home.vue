59a7626e9f2c13540844ccfd826223986f2d1d90
ArleneAndrews <kara_arlene@yahoo.com>
2018-04-27 11:59:36 -0600

    changes so Bash works

---
+++ WikiView/home.vue
@@ -0,0 +1,15 @@
+export default {
+  computed: {
+    username () {
+      // We will see what `params` is shortly
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  }
+}
