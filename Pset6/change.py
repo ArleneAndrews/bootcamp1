ce9f5fbca8d81777d25d80d38c15176d38031530
ArleneAndrews <kara_arlene@yahoo.com>
2018-04-28 13:11:05 -0600

    Change - works except for dollars

--- Pset6/change.py
+++ Pset6/change.py
@@ -5,11 +5,31 @@ if give < due:
     print('You need to get more money.')
 
 total = give - due
+total = round (total, 2)
+print(total)
 answer = ''
 
-if total >= 1.0
+if total >= 1.0:
     dollars = total/1.0
-    total = total - dollars
+    total = total  - int(dollars)
     answer = answer + str(dollars) +' dollars '
+
+if total >= .25:
+    quarters = total/25
+    total = total  - (quarters * 25)
+    answer = answer + str(int (quarters * 100)) +' quarters '
+
+if total >= .10:
+    dimes = total/10
+    total = total  - (dimes * 10)
+    answer = answer + str(int (dimes * 100)) +' dimes '
+
+if total >= .05:
+    nickles = total/5
+    total = total - (nickles * 5)
+    answer = answer + str(int(nickles * 100)) +' nickles '
+
+if total > 0:
+    answer = answer + str(int(total * 100)) +' pennies '
 
 print(answer)
