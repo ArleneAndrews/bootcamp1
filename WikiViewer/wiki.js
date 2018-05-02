f8c3b095e58428b0b6e6656bcf7db847e8265248
ArleneAndrews <kara_arlene@yahoo.com>
2018-05-02 13:23:58 -0600

    update to add comments

---
+++ WikiViewer/wiki.js
@@ -0,0 +1,10 @@
+function Random() {
+    let url = "https://en.wikipedia.org/wiki/Special:Random"
+// display url in results
+}
+
+function Search(term){
+    //regex for psces and punctuation
+    //isert into serach box
+    //display page in results
+}
