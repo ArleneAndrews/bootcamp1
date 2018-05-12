1b5342ea360899af83e853bc906dacd05ea28a5d
ArleneAndrews <kara_arlene@yahoo.com>
2018-05-12 11:25:16 -0600

    Well, I thought it worked

--- MyApp/CodementorCRUD/flask-crud-app/bookmanager.py
+++ MyApp/CodementorCRUD/flask-crud-app/bookmanager.py
@@ -1,10 +1,34 @@
+import os
+
 from flask import Flask
+from flask import render_template
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
-@app.route("/")
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "My flask app"
+    if request.form:
+        book = Book(title=request.form.get("title"))
+        db.session.add(book)
+        db.session.commit()
+        books = Book.query.all()
+    return render_template("home.html", books=books)
   
 if __name__ == "__main__":
     app.run(debug=True)
