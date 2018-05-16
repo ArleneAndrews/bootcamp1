e6a2d297be999952bc88c28322c85abd702c78ca
ArleneAndrews <kara_arlene@yahoo.com>
2018-05-16 11:32:08 -0600

    Location works- not happy with it, but...

--- MyApp/static/js/app.js
+++ MyApp/static/js/app.js
@@ -1,18 +1,1 @@
-function geoFindMe() {
-    $ ('results').empty();
-   var output = document.getElementById("detail");
-  
-   if (!navigator.geolocation){
-     output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-     return;
-   }
-  
-   function success(position) {
-     var latitude  = position.coords.latitude;
-     var longitude = position.coords.longitude;
-     document.getElementById("display").innerHTML = "Hello World";
-     return {lat:latitude, long: longitude}
-   }
-}
 
-document.getElementById("here").addEventListener("click", geoFindMe());
