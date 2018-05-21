a71e5093755c0bff5e67366f47558646a8e409ae
ArleneAndrews <kara_arlene@yahoo.com>
2018-05-21 15:58:14 -0600

    maybe?

--- WhatYouWant/app.py
+++ WhatYouWant/app.py
@@ -4,35 +4,41 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for, request, redirect
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, "whatyouwant.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "spot.db"))
+database_file2 = "sqlite:///{}".format(os.path.join(project_dir, "venue.db"))
 #Static file template
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
-
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file2
 db = SQLAlchemy(app)
 
-""" #Adding a spot to the database
+#Adding a spot to the database
 class Spot(db.Model):
     spotName =db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
     spotlat = db.Column(db.String(80), unique=True, nullable=False)
     spotlong = db.Column(db.String(80), unique=True,nullable=False)
+
+     def __init__(self, spotName, spotlat, spotlong):
+        self.spotName = spotName
+        self.spotlat = spotlat
+        self.spotlong = spotlong
     
     def __repr__(self):
-        return "<Spot: {}>".format(self.name, self.spotlat, .selfspotlong) 
+        return "<Spot: {}>".format(self.spotName) 
 
 #Adding a location for offline use 
-     class Venue(db.Model):
-        veuneName = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
-        venueLocation = db.Column(db.String(80), unique=True, nullable=False)
-        venuePhone = db.Column(db.String(80), unique=True, nullable=False)
-        venueVisit = db.Column(db.String(80), unique=True, nullable=False)
-        venueYelp = db.Column(db.String(200), unique=True)
-        venueStars =db.Column(db.String(80))
+class Venue(db.Model):
+    veuneName = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    venueLocation = db.Column(db.String(80), unique=True, nullable=False)
+    venuePhone = db.Column(db.String(80), unique=True, nullable=False)
+    venueVisit = db.Column(db.String(80), unique=True, nullable=False)
+    venueYelp = db.Column(db.String(200), unique=True)
+    venueStars =db.Column(db.String(80))
 
-        def __repr__(self):
-            return "<Venue: {}>".format(self.name, self.location, self.phone, self.visit, self.yelp, self.stars)
+    def __repr__(self):
+        return "<Venue: {}>".format(self.name, self.location, self.phone, self.visit, self.yelp, self.stars)
 
- """
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
