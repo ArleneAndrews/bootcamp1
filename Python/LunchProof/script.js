03ced6323b7eb857d75bbf3e82137a1234a82e7f
ArleneAndrews <kara_arlene@yahoo.com>
2018-06-04 16:39:39 -0600

    data transfer isn't what I want, but....

--- Python/LunchProof/script.js
+++ Python/LunchProof/script.js
@@ -1,7 +1,8 @@
 function geoFindMe() {
     var output = document.getElementById("out");
-  
-    if (!navigator.geolocation){
+    const distance = document.getElementById("distance").value;
+
+    if(!navigator.geolocation){
       output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
       return;
     }
@@ -13,7 +14,7 @@ function geoFindMe() {
       output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
   
       var img = new Image();
-      img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+      img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom="+ distance + "&size=300x300&sensor=false";
   
       output.appendChild(img);
     }
