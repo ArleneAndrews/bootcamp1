f0803bbff00d84bb49066a7bcf27b09fd21a00fb
ArleneAndrews <kara_arlene@yahoo.com>
2018-06-05 15:58:07 -0600

    places first attempt

--- Python/LunchProof/script.js
+++ Python/LunchProof/script.js
@@ -11,7 +11,7 @@ function geoFindMe() {
       var latitude  = position.coords.latitude;
       var longitude = position.coords.longitude;
   
-      output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+      //output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
   
       var img = new Image();
       img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom="+ distance + "&size=300x300&scale=2&sensor=false";
@@ -27,3 +27,39 @@ function geoFindMe() {
   
     navigator.geolocation.getCurrentPosition(success, error);
   }
+
+  //google place API key AIzaSyABQrtrls3Uqy1tHAbT4jepSdyLqoTNooM
+  function nearby()
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+
+  var map;
+var service;
+var infowindow;
+
+function initialize() {
+  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    type: ['restaurant']
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
