739213530c76fdebfa04a075a0b9e17d9a37af18
ArleneAndrews <kara_arlene@yahoo.com>
2018-06-06 12:13:33 -0600

    Kay doesn't work - time to look for options.

--- Python/LunchProof/script.js
+++ Python/LunchProof/script.js
@@ -14,14 +14,43 @@ function geoFindMe() {
       var latitude  = position.coords.latitude;
       var longitude = position.coords.longitude;
       
-      nearby();
-  
-      /* output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-  
-      var img = new Image();
-      img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom="+ distance + "&size=200x200&scale=2&sensor=false";
-  
-      output.appendChild(img);*/
+      function initMap() {
+        var pyrmont = {lat: latitude, lng: longitude};
+    
+        map = new google.maps.Map(document.getElementById('out'), {
+          center: pyrmont,
+          zoom: 15
+        });
+    
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: pyrmont,
+          radius: 500,
+          type: ['store']
+        }, callback);
+      }
+    
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+    
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+    
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
     }
   
     function error() {
@@ -34,38 +63,4 @@ function geoFindMe() {
   }
 
   //google place API key AIzaSyABQrtrls3Uqy1tHAbT4jepSdyLqoTNooM
-  function nearby()
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyABQrtrls3Uqy1tHAbT4jepSdyLqoTNooM&libraries=places&callback=initMap" async defer>
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-
-  var map;
-var service;
-var infowindow;
-
-function initialize() {
-  var pyrmont = new google.maps.LatLng(latitude,longitude);
-
-  map = new google.maps.Map(document.getElementById('out'), {
-      center: pyrmont,
-      zoom: 15
-    });
-
-  var request = {
-    location: pyrmont,
-    radius: distance,
-    type: ['restaurant']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
-}
+ 
