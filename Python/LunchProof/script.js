d564f79697ed9b7ef2204c467d436396cea7c895
ArleneAndrews <kara_arlene@yahoo.com>
2018-06-12 18:08:44 -0600

    update?

--- Python/LunchProof/script.js
+++ Python/LunchProof/script.js
@@ -1,74 +1,46 @@
-var defer = require('config/defer').deferConfig;
+/* var defer = require('config/defer').deferConfig;
 
 module.exports = {
    KEY : defer(function ()  {
     return this.key1+this.key2;
   })
-} 
+}  */
+function updateTextInput(val) {
+  document.getElementById('textInput').value=val; 
+  }
+
 document.getElementById("feedMe").addEventListener("click", geoFindMe());
 
 function geoFindMe() {
   
     var output = document.getElementById("out");
-    const distance = document.getElementById("distance").value;
+    var distance = document.getElementById("distance").value;
 
     if(!navigator.geolocation){
       output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
       return;
     }
   
-    function success(position) {
-     
+    function success(position) { 
       var latitude  = position.coords.latitude;
       var longitude = position.coords.longitude;
+      output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+      initMap();
+    }
       
-      function initMap() {
-        alert("WORKS!");
-        var pyrmont = {lat: latitude, lng: longitude};
-    
-        map = new google.maps.Map(document.getElementById('out'), {
-          center: pyrmont,
-          zoom: 15
-        });
-    
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: pyrmont,
-          radius: 500,
-          type: ['store']
-        }, callback);
-      }
-    
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
-    
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-    
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
-    }
-  
     function error() {
-      output.innerHTML = "Unable to retrieve your location";
+    output.innerHTML = "Unable to retrieve your location";
     }
-  
+    
     output.innerHTML = "<p>Locating…</p>";
   
     navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  function initMap() {
+    alert("WORKS!");
+    var places ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+latitude+","+longitude+"&radius="+distance+"&types=restaurant&key=supersecret"
+    console.log(places);
   }
 
  
