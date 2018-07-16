d18239b7c59976b2cd4738806b97b3fdfcc8155e
ArleneAndrews <kara_arlene@yahoo.com>
2018-07-16 10:21:42 -0600

    Test - login page loads correctly, comments on next ones

--- Python/Links/test.py
+++ Python/Links/test.py
@@ -10,6 +10,12 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
+    # Ensure that login page loads correctly
+    def test_login_page_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertTrue(b"Please log in" in response.data)    
+
 
 if __name__ == '__main__':
     unittest.main()
