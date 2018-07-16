173e5da4a0554e1ba3c2bd90de899f69e6505794
ArleneAndrews <kara_arlene@yahoo.com>
2018-07-16 10:22:10 -0600

    Now the correct save for test and comments

--- Python/Links/test.py
+++ Python/Links/test.py
@@ -14,8 +14,13 @@ class FlaskTestCase(unittest.TestCase):
     def test_login_page_loads(self):
         tester = app.test_client(self)
         response = tester.get('/login', content_type='html/text')
-        self.assertTrue(b"Please log in" in response.data)    
+        self.assertTrue(b"Please log in" in response.data)  
 
+    # Login - Correct credintials
+    
+    # Login - Wrong credintials  
+
+    # Logout
 
 if __name__ == '__main__':
     unittest.main()
