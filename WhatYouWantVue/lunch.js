51f4d891de579f8358b45606a330489368c67390
ArleneAndrews <kara_arlene@yahoo.com>
2018-07-25 09:02:38 -0600

    Cleanup, and trying for two input boxes

--- WhatYouWantVue/lunch.js
+++ WhatYouWantVue/lunch.js
@@ -1,19 +1,27 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
-  var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-  })
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
 
-  var app3 = new Vue({
-    el: '#app-3',
-    data: {
-      seen: true
-    }
-  })
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+function updateTextInputkm(val) {
+  document.getElementById('textInputkm').value=val; 
+  }
+
+function updateTextInputm(val) {
+  document.getElementById('textInputm').value=val; 
+  }
