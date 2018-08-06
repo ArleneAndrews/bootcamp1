6daf517397d82e96bbc0876646f59044f3f346b0
ArleneAndrews <kara_arlene@yahoo.com>
2018-08-06 12:09:30 -0600

    Pieces work, program doesn't.

--- Python/LunchProof/TestItOut/test.js
+++ Python/LunchProof/TestItOut/test.js
@@ -1,56 +1,86 @@
-//Globals
+//Globals, to be adjusted in calculations
 var lat = 0;
 var lon = 0;
 var far = 0;
+var zoom = 0;
 var buttonmode = "";
 var key = "";
-
-var box1=document.getElementById("skeleton");
-box1.onkeyup = function () {
-key = box1.value;
-}
-
-
-//slider and box match
 var slider = document.getElementById('distance');
-var box = document.getElementById("box");
 
-slider.onchange = function () {
-  box.value = slider.value;
+// Cheat for API key
+var box1 = document.getElementById("skeleton");
+box1.onkeyup = function () {
+  key = box1.value;
 }
-box.onkeyup = function () {
-  slider.value = box.value;
-}
-
-//finds correct function
-function mode(){
-  if (mode == 'feedMe') {
-    if(key === '') {
-      console.log ("No key!");
-      findMap();
-    }
-    console.log ("Find a spot!");
-    findSpots();
-  }
-  else {
-    console.log ("Show 'em all!");
-    findMap();
-  }
-};
-
 
 //Radius setting
 function territory() {
   far = slider.value;
   var output2 = document.getElementById("out2");
   var unit = document.querySelector('input[name="group1"]:checked').value;
+  console.log(unit);
   if (unit === "miles") {
     var miles = (far * 0.00062137119223733).toFixed(2)
     output2.innerHTML = '<p>Radius is ' + miles + ' miles </p>';
-  }
+    }
   var kms = (far / 1000).toFixed(2)
   output2.innerHTML = '<p>Radius is ' + kms + ' km </p>';
   mode();
+}
+
+
+//finds correct function
+function mode() {
+  if (mode == 'feedMe') {
+    if (key === '') {
+      console.log("No key!");
+      findMap();
+    }
+    console.log("Find a spot!");
+    findSpots();
+  } else {
+    console.log("Show 'em all!");
+    findMap();
+  }
+};
+
+//set zoom for map
+function zoomLevel() {
+  switch (far) {
+    case (far < 1500):
+      zoom = 20;
+      break;
+    case (far < 5000):
+      zoom = 19;
+      break;
+    case (far < 10000):
+      zoom = 18;
+      break;
+    case (far < 15000):
+      zoom = 17;
+      break;
+    case (far < 20000):
+      zoom = 16;
+      break;
+    case (far < 25000):
+      zoom = 15;
+      break;
+    case (far < 30000):
+      zoom = 14;
+      break;
+    case (far < 35000):
+      zoom = 13;
+      break;
+    case (far < 40000):
+      zoom = 12;
+      break;
+    case (far < 45000):
+      zoom = 11;
+      break;
+    case (far < 40000):
+      zoom = 10;
+      break;
+  }
 }
 
 function geoFindMe(source) {
@@ -81,19 +111,19 @@ function geoFindMe(source) {
 function findSpots() {
   console.log("It works thus far!");
 };
-  /*if (key == "") {
-    findMap()
-    return;
-  }
-  geoFindMe();
-  territory();
-  //TODO: change these to match incoming 
-  lat = position[0];
-  lon = position[1];
+/*if (key == "") {
+  findMap()
+  return;
+}
+geoFindMe();
+territory();
+//TODO: change these to match incoming 
+lat = position[0];
+lon = position[1];
 
-  var output = document.getElementById("out2");
-  output.innerHTML = "<p>Works this far!</p>";*/
-  /*var places =  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lon + "&radius=" + far + "&types=restaurant&key=" + key;
+var output = document.getElementById("out2");
+output.innerHTML = "<p>Works this far!</p>";*/
+/*var places =  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lon + "&radius=" + far + "&types=restaurant&key=" + key;
   return fetch(places, {
       mode: 'no-cors'
     })
@@ -115,12 +145,14 @@ function findSpots() {
 
 function findMap() {
   var mydiv = document.getElementById('out3');
-while(mydiv.firstChild) {
-  mydiv.removeChild(mydiv.firstChild);
-}
+  while (mydiv.firstChild) {
+    mydiv.removeChild(mydiv.firstChild);
+  }
+  zoomLevel();
+  console.log(zoom);
   var img = new Image();
   var output = document.getElementById("out3");
-  img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=13&size=300x300&sensor=false";
+  img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=" + zoom + "&size=300x300&sensor=false";
 
   output.appendChild(img);
 };
