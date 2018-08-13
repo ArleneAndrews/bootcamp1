41bd148530904d2601a5014b2dc593c731896c5e
ArleneAndrews <kara_arlene@yahoo.com>
2018-08-13 17:51:49 -0600

    trying to match - no workie.

--- TutorialWork/your-first-pwapp-master/your-first-pwapp-master/work/service.-worker.js
+++ TutorialWork/your-first-pwapp-master/your-first-pwapp-master/work/service.-worker.js
@@ -1,12 +1,12 @@
-var cacheName = "weatherPWA-Step-6-1";
+var cacheName = "weatherPWA-step-6-1";
 var filestoCache = [];
 
 self.addEventListener('install', function(e){
 console.log('[ServiceWorker] Install');
 e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-        console.log('[ServiceWorker] Cacheing app shell');
+        console.log('[ServiceWorker] Caching app shell');
         return cache.addAll(filestoCache);
-    })
-);
+        })
+    );
 });
